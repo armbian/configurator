8981112fc242d472ac86d4546cf35ce5604c78ea
Myy Miouyouyou <myy@miouyouyou.fr>
2022-06-06 04:11:46 +0200

    Starting to get a decent configurator
    * Loading translated descriptions automatically is now working.
    * The configurator will try to load the GUI or TUI version of
      a module if no arguments were provided.
    * The module arguments are now passed correctly...
    
    Docker module :
    * The GUI now has a CSS file
    * The GUI now has translations files
    * The dependencies have been updated to include docker, docker-compose
      and also libpyside2-dev for the QT version
    
    So we're starting to get something decently working, and
    which parts can be updated independently.
    
    Testing still needs to be done, but the example now provide
    a nice overview of how you can ship a text and UI version of
    a module.
    
    Signed-off-by: Myy Miouyouyou <myy@miouyouyou.fr>

---
+++ modules/docker/ui/mainwindow/translations/fr.ts
@@ -0,0 +1,82 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>ComposeManagerWindow</name>
+    <message>
+        <location filename="../modulewindow_qt.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation>Fenêtre principale</translation>
+    </message>
+    <message>
+        <location filename="../modulewindow_qt.ui" line="23"/>
+        <source>Installed</source>
+        <translation>Services installés</translation>
+    </message>
+    <message>
+        <location filename="../modulewindow_qt.ui" line="46"/>
+        <source>Available</source>
+        <translation>Tous les services</translation>
+    </message>
+    <message>
+        <location filename="../modulewindow_qt.ui" line="91"/>
+        <source>TextLabel</source>
+        <translation>Intitulé du service</translation>
+    </message>
+    <message>
+        <location filename="../modulewindow_qt.ui" line="115"/>
+        <source>Up</source>
+        <translation>Up</translation>
+    </message>
+    <message>
+        <location filename="../modulewindow_qt.ui" line="140"/>
+        <source>Down</source>
+        <translation>Down</translation>
+    </message>
+    <message>
+        <location filename="../modulewindow_qt.ui" line="172"/>
+        <source>Edit</source>
+        <translation>YAML</translation>
+    </message>
+    <message>
+        <location filename="../modulewindow_qt.ui" line="179"/>
+        <source>Open folder</source>
+        <translation>Dossier</translation>
+    </message>
+    <message>
+        <location filename="../modulewindow_qt.ui" line="214"/>
+        <source>Uninstall</source>
+        <translation>Désinstaller</translation>
+    </message>
+    <message>
+        <location filename="../modulewindow_qt.ui" line="245"/>
+        <source>Install</source>
+        <translation>Installer le service</translation>
+    </message>
+    <message>
+        <location filename="../modulewindow_qt.ui" line="257"/>
+        <source>Operations output</source>
+        <translation>Messages des opérations</translation>
+    </message>
+    <message>
+        <location filename="../modulewindow_qt.ui" line="280"/>
+        <source>Status</source>
+        <translation>Statut du service</translation>
+    </message>
+    <message>
+        <location filename="../modulewindow_qt.ui" line="294"/>
+        <source>YAML</source>
+        <translation>docker-compose.yml</translation>
+    </message>
+    <message>
+        <location filename="../modulewindow_qt.ui" line="322"/>
+        <source>File</source>
+        <translation>Fichier</translation>
+    </message>
+    <message>
+        <location filename="../modulewindow_qt.ui" line="331"/>
+        <source>Quit</source>
+        <translation>Quitter</translation>
+    </message>
+</context>
+</TS>
---
+++ modules/docker/ui/mainwindow/translations/zh.ts
@@ -0,0 +1,87 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>ComposeManagerWindow</name>
+    <message>
+        <location filename="../modulewindow_qt.ui" line="14"/>
+        <source>MainWindow</source>
+        <translatorcomment>DeepL Translation from Japanese</translatorcomment>
+        <translation>主屏幕</translation>
+    </message>
+    <message>
+        <location filename="../modulewindow_qt.ui" line="23"/>
+        <source>Installed</source>
+        <translatorcomment>DeepL Translation from Japanese</translatorcomment>
+        <translation>已安装的服务</translation>
+    </message>
+    <message>
+        <location filename="../modulewindow_qt.ui" line="46"/>
+        <source>Available</source>
+        <translatorcomment>DeepL Translation from Japanese</translatorcomment>
+        <translation>全部服务</translation>
+    </message>
+    <message>
+        <location filename="../modulewindow_qt.ui" line="91"/>
+        <source>TextLabel</source>
+        <translatorcomment>DeepL Translation from Japanese</translatorcomment>
+        <translation>所选服务的名称</translation>
+    </message>
+    <message>
+        <location filename="../modulewindow_qt.ui" line="115"/>
+        <source>Up</source>
+        <translation>Up</translation>
+    </message>
+    <message>
+        <location filename="../modulewindow_qt.ui" line="140"/>
+        <source>Down</source>
+        <translation>Down</translation>
+    </message>
+    <message>
+        <location filename="../modulewindow_qt.ui" line="172"/>
+        <source>Edit</source>
+        <translation>编辑</translation>
+    </message>
+    <message>
+        <location filename="../modulewindow_qt.ui" line="179"/>
+        <source>Open folder</source>
+        <translation>文件夹</translation>
+    </message>
+    <message>
+        <location filename="../modulewindow_qt.ui" line="214"/>
+        <source>Uninstall</source>
+        <translatorcomment>Deepl Translation of 削除</translatorcomment>
+        <translation>删减</translation>
+    </message>
+    <message>
+        <location filename="../modulewindow_qt.ui" line="245"/>
+        <source>Install</source>
+        <translation>安装</translation>
+    </message>
+    <message>
+        <location filename="../modulewindow_qt.ui" line="257"/>
+        <source>Operations output</source>
+        <translation>执行结果</translation>
+    </message>
+    <message>
+        <location filename="../modulewindow_qt.ui" line="280"/>
+        <source>Status</source>
+        <translation>服务状况</translation>
+    </message>
+    <message>
+        <location filename="../modulewindow_qt.ui" line="294"/>
+        <source>YAML</source>
+        <translation>docker-compose.yml</translation>
+    </message>
+    <message>
+        <location filename="../modulewindow_qt.ui" line="322"/>
+        <source>File</source>
+        <translation>文件</translation>
+    </message>
+    <message>
+        <location filename="../modulewindow_qt.ui" line="331"/>
+        <source>Quit</source>
+        <translation>结束</translation>
+    </message>
+</context>
+</TS>
